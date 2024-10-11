9b22f9853bd04872dd01574b95cccd8214ab6caa
Глеб Кот <116066796+memedea1er@users.noreply.github.com>
2024-10-11 21:26:03 +0500

    added blog

---
+++ lab4_gpt4_react/src/mockData/blogData.js
@@ -0,0 +1,50 @@
+import insideMatrixCableImg from "../assets/img/inside_matrix_cable.png";
+import matrixFaceImg from "../assets/img/matrix_face.png";
+import matrixImg from "../assets/img/matrix.png";
+import matrixLinkImg from "../assets/img/matrix_link.png";
+import matrixAlertImg from "../assets/img/matrix_alert.png";
+
+export const blogHeader = "Многое Происходит, Мы Ведем об Этом Блог.";
+
+const squares = [
+  {
+    imgSrc: insideMatrixCableImg,
+    date: "Сент 26, 2021",
+    content: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+    type: 'square1',
+  },
+  {
+    imgSrc: matrixFaceImg,
+    date: "Сент 26, 2021",
+    content: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+    type: 'square2_1',
+  },
+  {
+    imgSrc: matrixImg,
+    date: "Сент 26, 2021",
+    content: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+    type: 'square2_2',
+  },
+  {
+    imgSrc: matrixLinkImg,
+    date: "Сент 26, 2021",
+    content: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+    type: 'square3_1',
+  },
+  {
+    imgSrc: matrixAlertImg,
+    date: "Сент 26, 2021",
+    content: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+    type: 'square3_2',
+  },
+];
+
+export const squaresData = {
+  blogHeader,
+  squares,
+};
